a7ef98cbebd3be2dcabf5ff078c80c1b0bd92318
Razieh Abedi <raziyeabedi@gmail.com>
2025-04-19 15:09:05 +0800

    created the project

---
+++ .next/server/pages/new-meetup.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/new-meetup";
+exports.ids = ["pages/new-meetup"];
+exports.modules = {
+
+/***/ "./pages/new-meetup/index.js":
+/*!***********************************!*\
+  !*** ./pages/new-meetup/index.js ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n\nconst NewMeetup = ()=>{\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"div\", {\n        children: \"new meetup\"\n    }, void 0, false, {\n        fileName: \"C:\\\\Users\\\\User\\\\Documents\\\\personal-projects\\\\meetup-next-app\\\\pages\\\\new-meetup\\\\index.js\",\n        lineNumber: 2,\n        columnNumber: 10\n    }, undefined);\n};\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (NewMeetup);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9uZXctbWVldHVwL2luZGV4LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQUEsTUFBTUEsU0FBUyxHQUFHLElBQU07SUFDdEIscUJBQU8sOERBQUNDLEtBQUc7a0JBQUMsWUFBVTs7Ozs7aUJBQU0sQ0FBQztBQUMvQixDQUFDO0FBQ0QsaUVBQWVELFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL25leHRqcy1jb3Vyc2UvLi9wYWdlcy9uZXctbWVldHVwL2luZGV4LmpzPzk1ODIiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgTmV3TWVldHVwID0gKCkgPT4ge1xyXG4gIHJldHVybiA8ZGl2Pm5ldyBtZWV0dXA8L2Rpdj47XHJcbn07XHJcbmV4cG9ydCBkZWZhdWx0IE5ld01lZXR1cDtcclxuIl0sIm5hbWVzIjpbIk5ld01lZXR1cCIsImRpdiJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/new-meetup/index.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/new-meetup/index.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,4 @@
+const NewMeetup = () => {
+  return <div>new meetup</div>;
+};
+export default NewMeetup;
