48b8210dad4a82e2196ab9683772f353a6e856d1
Razieh Abedi <raziyeabedi@gmail.com>
2025-04-20 11:33:43 +0800

    added custom detail component with css modlues

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,5 +1,15 @@
-const DetailsPage = () => {
-  return <div>Details page</div>;
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      id="m1"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Chehel_Sotoun_isfahan.jpg/960px-Chehel_Sotoun_isfahan.jpg"
+      title="The first meetup"
+      address="First meetup address"
+      description="This is the first meetup description"
+    />
+  );
 };
 
-export default DetailsPage;
+export default MeetupDetails;
