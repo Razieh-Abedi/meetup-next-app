2b556ed3dbce2a94ef082c3239a62951e1b99683
Razieh Abedi <raziyeabedi@gmail.com>
2025-04-21 14:06:04 +0800

    fetch the data for a specific id

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,48 +1,52 @@
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      id="m1"
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Chehel_Sotoun_isfahan.jpg/960px-Chehel_Sotoun_isfahan.jpg"
-      title="The first meetup"
-      address="First meetup address"
-      description="This is the first meetup description"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
+  const db = client.db();
+  const meetupCollection = db.collection("Cluster0");
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+  client.close();
+
   return {
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
     fallback: false,
   };
 }
 
 export async function getStaticProps(context) {
-  ///Fetch data for a single meetup
   const meetupId = context.params.meetupId;
+  ///Fetch data for a single meetup
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
+  const db = client.db();
+  const meetupCollection = db.collection("Cluster0");
+  const selectedMeetup = await meetupCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+  client.close();
 
   return {
     props: {
       meetupData: {
-        id: meetupId,
-        image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Chehel_Sotoun_isfahan.jpg/960px-Chehel_Sotoun_isfahan.jpg",
-        title: "The first meetup",
-        address: "First meetup address",
-        description: "This is the first meetup description",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        image: selectedMeetup.image,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
